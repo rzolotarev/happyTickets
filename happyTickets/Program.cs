95bd5d8db548dd92da49484ff94a552507cf2dc2
Roman.Zolotarev <mirom90@gmail.com>
2022-12-11 21:06:30 +0300

    Add project files.

---
+++ happyTickets/Program.cs
@@ -0,0 +1,27 @@
+﻿
+var length = 6;
+var measure = 12;
+var sums = new long[length + 1, length * measure + 1];
+for (var i = 0; i <= measure; i++)
+    sums[1, i] = 1;
+
+for (var i = 2; i <= length; i++)
+{
+    for (var s = 0; s <= i * measure; s++)
+    {
+        for (var k = 0; k <= s && k <= measure; k++)
+        {
+            sums[i, s] += sums[i - 1, s - k];
+        }
+    }
+}
+
+var happyTickets = 0L;
+for (var i = 0; i <= length * measure; i++)
+{
+    happyTickets += (sums[length, i] * sums[length, i]);
+}
+
+Console.WriteLine(happyTickets);
+
+//707 972 099 627
